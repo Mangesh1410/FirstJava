04719a7300ff95da60c9f17fc577ce6fec8af668
metkarimangesh@gmail.com <metkarimangesh@gmail.com>
2018-07-10 23:01:20 -0700

    Code Updated

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -5,11 +5,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-	
-	@RequestMapping(path="/welcome")
+
+	@RequestMapping(path = "/welcome")
 	public String getMessage() {
 
-		
-	return "Welcome  To Java.";
-}
+		return "Welcome  To Java.";
 	}
+
+	@RequestMapping(path = "/mobinext")
+	public String getWelcomeToMN() {
+
+		return "<h2>Welcome  To MobiNext Tech.</h2>";
+	}
+}
